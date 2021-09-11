b0c46ac3dcc2f955380a225d297a442486fb946b
pgalias <pawel@galias.me>
2021-09-11 22:58:04 +0200

    feat: create simple endpoints in Server Service
    They are connecting with Search Service and fetching data from there

--- packages/server/src/app.controller.ts
+++ packages/server/src/app.controller.ts
@@ -1,12 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Controller,
+  Get,
+  Query,
+  ParseIntPipe,
+  ParseFloatPipe,
+  ParseArrayPipe,
+} from '@nestjs/common';
+import { ClientProxy, ClientProxyFactory } from '@nestjs/microservices';
+import { microserviceOptions } from '@foodby/search';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private readonly client: ClientProxy;
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  constructor() {
+    this.client = ClientProxyFactory.create(microserviceOptions);
+  }
+
+  @Get('restaurants')
+  getRestaurants(
+    @Query('latitude', ParseFloatPipe) latitude: number,
+    @Query('longitude', ParseFloatPipe) longitude: number,
+    @Query('radius', ParseIntPipe) radius: number,
+    @Query('cuisineTypes', ParseArrayPipe) cuisineTypes: string[],
+  ) {
+    return this.client.send('restaurants', [
+      { latitude: latitude, longitude: longitude },
+      radius,
+      cuisineTypes,
+    ]);
+  }
+
+  @Get('cuisine-types')
+  getCuisineTypes() {
+    return this.client.send('cuisine-type', []);
   }
 }
