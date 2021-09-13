25022916cf9d54395c06a4561af70013e1d85c42
pgalias <pawel@galias.me>
2021-09-14 00:19:18 +0200

    chore(ccu): clean up the package

---
+++ packages/common-components-ui/src/components/index.ts
@@ -0,0 +1,5 @@
+export * from './alert';
+export * from './card';
+export * from './input';
+export * from './selectable-tag';
+export * from './slider';
--- packages/common-components-ui/src/main.tsx
+++ packages/common-components-ui/src/main.tsx
@@ -1,11 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+export * from './components';
+export * from './icons';
