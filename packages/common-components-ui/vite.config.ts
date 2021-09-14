ce72f0f6372ccd615c616634ef5cb7f2d8cdd997
pgalias <pawel@galias.me>
2021-09-15 01:46:07 +0200

    chore(ccu): make library exportable

--- packages/common-components-ui/vite.config.ts
+++ packages/common-components-ui/vite.config.ts
@@ -1,7 +1,51 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import typescript from '@rollup/plugin-typescript';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
-})
+  plugins: [reactRefresh()],
+  build: {
+    lib: {
+      entry: 'src/main.ts',
+      name: '@foodby/common-components-ui',
+      fileName: (format) => `main.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react'],
+      output: {
+        globals: {
+          react: 'React',
+        },
+      },
+      plugins: [
+        // @ts-ignore
+        typescript({
+          target: 'ESNext',
+          useDefineForClassFields: true,
+          lib: ['DOM', 'DOM.Iterable', 'ESNext'],
+          allowJs: false,
+          skipLibCheck: true,
+          esModuleInterop: true,
+          allowSyntheticDefaultImports: true,
+          strict: true,
+          forceConsistentCasingInFileNames: true,
+          module: 'ESNext',
+          moduleResolution: 'Node',
+          resolveJsonModule: true,
+          isolatedModules: true,
+          declaration: true,
+          declarationDir: './dist',
+          jsx: 'react',
+          exclude: [
+            'node_modules',
+            './src/**/*.stories.tsx',
+            './src/**/*.test.tsx',
+            './src/__mocks__/**/*.ts',
+            './src/setupTests.ts',
+          ],
+        }),
+      ],
+    },
+  },
+});
