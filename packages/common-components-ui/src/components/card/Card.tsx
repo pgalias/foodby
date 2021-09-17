b6ddb43c5311db6dd44f7fecc74478f4c7cad92b
pgalias <pawel@galias.me>
2021-09-18 01:26:35 +0200

    fix(ccu): add className prop to Card component

--- packages/common-components-ui/src/components/card/Card.tsx
+++ packages/common-components-ui/src/components/card/Card.tsx
@@ -2,17 +2,19 @@ import React, { FC } from 'react';
 import clsx from 'clsx';
 
 interface CardProps {
+  className?: string;
   children: React.ReactNode;
 }
 
-export const Card: FC<CardProps> = ({ children }) => (
+export const Card: FC<CardProps> = ({ className, children }) => (
   <section
     className={clsx(
       'fb-card',
-      'w-4/6 p-0',
+      'w-2/5 p-8',
       'bg-white text-blueGray-600',
       'rounded-lg',
       'overflow-hidden',
+      className,
     )}
   >
     {children}
