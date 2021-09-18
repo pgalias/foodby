d68d6f9f08b9ef8d125d0bc16049a4c1372cb9dd
pgalias <pawel@galias.me>
2021-09-19 00:32:43 +0200

    fix(ccu): make Card component more responsive

--- packages/common-components-ui/src/components/card/Card.tsx
+++ packages/common-components-ui/src/components/card/Card.tsx
@@ -10,7 +10,7 @@ export const Card: FC<CardProps> = ({ className, children }) => (
   <section
     className={clsx(
       'fb-card',
-      'w-2/5 p-8',
+      'w-11/12 md:w-3/5 lg:w-2/5 p-8',
       'bg-white text-blueGray-600',
       'rounded-lg',
       'overflow-hidden',
